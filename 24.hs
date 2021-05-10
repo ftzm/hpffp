e736831f8b626c46ec7e9da436b7a6d54dd40019
ftzm <m@ftzm.org>
2021-05-10 08:36:20 +0200

    pargras

---
+++ 24.hs
@@ -0,0 +1,5 @@
+module Chapter24 where
+
+-- I've been using parser combinators for a good while already. I could have
+-- used some more theory on how parsers work or on alternative as a typeclass,
+-- but this was not that. Skipped.
---
+++ 25.hs
@@ -0,0 +1,73 @@
+{-# LANGUAGE InstanceSigs #-}
+
+module Chapter25 where
+
+import Prelude hiding (fold)
+import Data.Foldable
+
+newtype Compose f g a = Compose { getCompose :: f (g a) }
+  deriving (Eq, Show)
+
+instance (Functor f, Functor g) => Functor (Compose f g) where
+  fmap f (Compose fga) = Compose $ (fmap . fmap) f fga
+
+instance (Applicative f, Applicative g) => Applicative (Compose f g) where
+  pure :: a -> Compose f g a
+  pure = Compose . pure . pure
+
+  (<*>) :: Compose f g (a -> b) -> Compose f g a -> Compose f g b
+  (Compose f) <*> (Compose a) = Compose $ (<*>) <$> f <*> a
+
+instance (Foldable f, Foldable g) => Foldable (Compose f g) where
+  foldMap f (Compose a) = foldMap (foldMap f) a
+
+instance (Traversable f, Traversable g) => Traversable (Compose f g) where
+  traverse f (Compose a)= Compose <$> traverse (traverse f) a
+
+--------------------------------------------------------------------------------
+-- And now for something completely different
+
+class Bifunctor p where
+  {-# MINIMAL bimap | first, second #-}
+  bimap :: (a -> b) -> (c -> d) -> p a c -> p b d
+  bimap f g = first f . second g
+
+  first :: (a -> b) -> p a c -> p b c
+  first f = bimap f id
+
+  second :: (b -> c) -> p a b -> p a c
+  second = bimap id
+
+data Deux a b = Deux a b
+
+instance Bifunctor Deux where
+  bimap f g (Deux a b) = Deux (f a) (g b)
+
+data Const a b = Const a
+
+instance Bifunctor Const where
+  bimap f _ (Const a) = Const $ f a
+
+data Drei a b c = Drei a b c
+
+instance Bifunctor (Drei a) where
+  bimap f g (Drei a b c) = Drei a (f b) (g c)
+
+data SuperDrei a b c = SuperDrei a b
+
+instance Bifunctor (SuperDrei a) where
+  bimap f _ (SuperDrei a b) = SuperDrei a (f b)
+
+data SemiDrei a b c = SemiDrei a
+
+instance Bifunctor (SemiDrei a) where
+  bimap _ _ (SemiDrei a) = SemiDrei a
+
+data Quadriceps a b c d = Quadzzz a b c d
+
+instance Bifunctor (Quadriceps a b) where
+  bimap f g (Quadzzz a b c d) = Quadzzz a b (f c) (g d)
+
+instance Bifunctor Either where
+  bimap f _ (Left a) = Left $ f a
+  bimap _ g (Right b) = Right $ g b
